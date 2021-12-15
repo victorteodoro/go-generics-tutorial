ebc9593c18e38866f99e4b21d51cadf535cf801b
Victor Teodoro <victorjst@hotmail.com>
2021-12-15 15:55:10 -0300

    code: add first generic code example

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := map[string]int64{
+		"1st": 32,
+		"2nd": 12,
+	}
+
+	floats := map[string]float64{
+		"1st": 32.13,
+		"2nd": 12.15,
+	}
+
+	fmt.Printf("Non-generic sums: %v and %v.\n",
+		SumInts(ints),
+		SumFloats(floats))
+
+	fmt.Printf("Generic sums: %v and %v.\n",
+		SumGeneric(ints),
+		SumGeneric(floats))
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+func SumGeneric[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
